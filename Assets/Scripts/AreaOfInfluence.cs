fb9c4cf96174ffbac24eb18c07edd3adec812d97
Sean Blevins <sean.blevins256@gmail.com>
2015-03-21 11:46:56 +0000

    TriColorMix
    Color mix works with 3 players.
    Colors of players and platforms can be selected with a public Enum

--- Assets/Scripts/AreaOfInfluence.cs
+++ Assets/Scripts/AreaOfInfluence.cs
@@ -1,31 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AreaOfInfluence : MonoBehaviour {
 
     GameManager GM;
 
-    Color baseColor;
-    Color curColor;
-    platform curPlatform = null;
+    public Color baseColor;
+    public Color curColor;
 
-    ArrayList platforms = new ArrayList();
+    List<platform> platforms = new List<platform>();
+    List<character> linkedCharacters = new List<character>();
 
 	// Use this for initialization
 	void Start () {
 
         GM = GameManager.Instance;
 
-        baseColor = GetComponentInParent<character>().baseColor;
-        curColor = baseColor;
+        //baseColor = GetComponentInParent<character>().baseColor;
+        //curColor = baseColor;
 
-        GetComponent<SpriteRenderer>().color = curColor;
+        //<SpriteRenderer>().color = curColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void initColor(Color initColor)
+    {
+        baseColor = initColor;
+        curColor = baseColor;
+
+        GetComponent<SpriteRenderer>().color = curColor;
+    }
 
     void OnTriggerEnter(Collider colliderInfo)
     {
@@ -39,9 +48,15 @@ public class AreaOfInfluence : MonoBehaviour {
         if (colliderInfo.GetComponent<Collider>().tag == "AreaTrigger")
         {
             //Combine colors
-            GM.characters.Add(GetComponentInParent<character>());
-            GM.colors.Add(curColor);
-            GM.updateCharacters();
+            linkedCharacters.Add(colliderInfo.GetComponent<character>());
+            if (!GM.characters.Contains(GetComponentInParent<character>()))
+            {
+                print(baseColor);
+                GM.characters.Add(GetComponentInParent<character>());
+                GM.colors.Add(baseColor);
+                GM.updateCharacters();
+            }
+            
             //GM.addToGroup(GetComponentInParent<character>());
         }
 
@@ -69,8 +84,16 @@ public class AreaOfInfluence : MonoBehaviour {
 
         if (colliderInfo.GetComponent<Collider>().tag == "AreaTrigger")
         {
-            print("Break up");
-            GM.removeFromGroup(GetComponentInParent<character>());
+            print("Break link");
+            
+            linkedCharacters.Remove(colliderInfo.GetComponent<character>());
+
+            if (linkedCharacters.Count == 0)
+            {
+                print("Remove from group");
+                GM.removeFromGroup(GetComponentInParent<character>());
+                
+            }
             //GM.characters.Remove(GetComponentInParent<character>());
             //GM.colors.Remove(curColor);
             //GM.updateCharacters();
@@ -86,9 +109,8 @@ public class AreaOfInfluence : MonoBehaviour {
             curColor = newColor;
             GetComponent<SpriteRenderer>().color = curColor;
 
-            foreach (var item in platforms)
+            foreach (var platform in platforms)
             {
-                platform platform = (platform)item;
                 platform.activatePlatform(curColor);
             }
         }
--- Assets/Scripts/platform.cs
+++ Assets/Scripts/platform.cs
@@ -3,27 +3,58 @@ using System.Collections;
 
 public class platform : MonoBehaviour {
 
+    public enum activateColorEnum
+    {
+        RED = 1,
+        BLUE,
+        YELLOW,
+        PURPLE,
+        ORANGE,
+        GREEN,
+        WHITE
+    }
 
     bool active = false;    
     public Color activateColor;
 
+    public activateColorEnum activateColorEn;
+
 	// Use this for initialization
 	void Start () {
+
+        switch (activateColorEn)
+        {
+            case activateColorEnum.RED:
+                activateColor = Color.red;
+                break;
+            case activateColorEnum.BLUE:
+                activateColor = Color.blue;
+                break;
+            case activateColorEnum.YELLOW:
+                activateColor = Color.yellow;
+                break;
+            case activateColorEnum.PURPLE:
+                activateColor = new Color(0.7f, 0f, 1f, 1f);
+                break;
+            case activateColorEnum.ORANGE:
+                activateColor = new Color(1f, 0.65f, 0f, 1f);
+                break;
+            case activateColorEnum.GREEN:
+                activateColor = Color.green;
+                break;
+            case activateColorEnum.WHITE:
+                activateColor = Color.white;
+                break;
+            default:
+                break;
+        }
+
+        deactivatePlatform();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (active)
-        {
-            
-            GetComponent<Renderer>().material.color = Color.white;            
-        }
-        else
-        {
-            GetComponent<Renderer>().material.color = activateColor;            
-        }
 	
 	}
 
@@ -33,11 +64,17 @@ public class platform : MonoBehaviour {
         {            
             active = true;
             GetComponent<BoxCollider>().enabled = true;
+            GetComponent<Renderer>().material.color = Color.white;
+            Color trans = new Color();
+            trans = GetComponent<Renderer>().material.color;
+            trans.a = 0.25f;
+            GetComponent<Renderer>().material.color = trans;
         }
         else
         {
             active = false;
             GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Renderer>().material.color = activateColor;
         }
     }
 
@@ -45,5 +82,6 @@ public class platform : MonoBehaviour {
     {       
         active = false;
         GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Renderer>().material.color = activateColor;
     }
 }
